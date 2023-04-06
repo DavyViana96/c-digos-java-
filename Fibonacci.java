4ae92852aeb265d2dba69a5b309194280d9eee0a
DavyViana96 <130017924+DavyViana96@users.noreply.github.com>
2023-04-05 23:52:42 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,11 +4,11 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        // Recebe o número do usuário
+     
         System.out.print("Digite um número inteiro: ");
         int num = input.nextInt();
         
-        // Verifica se o número pertence à sequência de Fibonacci
+        
         int a = 0, b = 1, c;
         while (b < num) {
             c = a + b;
